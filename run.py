41c4455aa44c431c0a3280ee7bf985eecf05f7cc
DominikBBB <mrdomob@gmail.com>
2023-02-13 16:12:51 +0000

    Import colorama styles, import sleep function, create clear terminal function

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import random
 
 import colorama
 from colorama import Fore, Style
+from time import sleep
 
 from words import words
 from stages import hangman
@@ -12,22 +13,28 @@ from pprint import pprint
 
 
 
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def welcome():
     """
     Welcome message
     """
     print('*' * 70)
     print(Fore.YELLOW + "Welcome to the Hangman Game!\n")
-    print(Fore.RED + "How do you want to hang today?\n")
+    print(Fore.RED + "Do you know how you want to hang today?\n")
     print(Style.RESET_ALL + '*' * 70)
-    print('  +---+'+'  +---+'+'  +---+')
-    print('  |   |'+'  |   |'+'  |   |')
-    print('  O   |'+'  O   |'+'  O   |')
-    print(' /|\  |'+' /|\  |'+' /|\  |')
-    print(' / \  |'+' / \  |'+' / \  |')
-    print('      |'+'      |'+'      |')
+    print(Fore.RED + '  +---+'+'  +---+'+'  +---+')
+    print(Fore.RED + '  |   |'+'  |   |'+'  |   |')
+    print(Fore.RED + '  O   |'+'  O   |'+'  O   |')
+    print(Fore.RED + ' /|\  |'+' /|\  |'+' /|\  |')
+    print(Fore.RED + ' / \  |'+' / \  |'+' / \  |')
+    print(Fore.RED + '      |'+'      |'+'      |')
     print(Fore.GREEN + '========='+'========='+'=========')
-    print('*' * 70)
+    print(Style.RESET_ALL + '*' * 70)
+    sleep(3)
+
 
 
 welcome()
