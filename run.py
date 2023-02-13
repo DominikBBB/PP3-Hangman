6948863e12179b8f35b0d2ffc3ca2f164c8219af
DominikBBB <mrdomob@gmail.com>
2023-02-13 15:45:19 +0000

    Create welcome function

--- run.py
+++ run.py
@@ -1,8 +1,41 @@
 import random
+
+import colorama
+from colorama import Fore
+
 from words import words
 from stages import hangman
+
 import os
 
 from pprint import pprint
 
-print('Hello World!')
+
+
+def welcome():
+    """
+    Welcome message
+    """
+    print('*' * 70)
+    print("Welcome to the Hangman Game!\n")
+    print("How do you want to hang today?\n")
+    print('*' * 70)
+    print('  +---+'+'  +---+'+'  +---+')
+    print('  |   |'+'  |   |'+'  |   |')
+    print('  O   |'+'  O   |'+'  O   |')
+    print(' /|\  |'+' /|\  |'+' /|\  |')
+    print(' / \  |'+' / \  |'+' / \  |')
+    print('      |'+'      |'+'      |')
+    print('========='+'========='+'=========')
+    print('*' * 70)
+
+
+
+
+
+
+
+welcome()
+
+
+
